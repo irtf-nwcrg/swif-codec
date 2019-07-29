67243e45a067bc91741829eea312e8b2128ca778
Cedric Adjih <cedric.adjih@inria.fr>
2019-07-29 00:01:03 -0700

    added more code for python testing

--- wrapper/python/test_codec.py
+++ wrapper/python/test_codec.py
@@ -6,7 +6,8 @@ from collections import namedtuple
 import swif
 
 SYMBOL_SIZE = 16
-MAX_CODING_WINDOW_SIZE = 4
+MAX_CODING_WINDOW_SIZE = 2
+NB_WINDOW = 2
 
 Packet = namedtuple("Packet", "first_id nb_id data key")
 
@@ -19,20 +20,21 @@ def make_source_symbol(symbol_id):
 def try_encoder():
     packet_list = []
     encoder = swif.RlcEncoder(SYMBOL_SIZE, MAX_CODING_WINDOW_SIZE)
-    for i in range(MAX_CODING_WINDOW_SIZE):
-        symbol = make_source_symbol(i)
-        encoder.add_source_symbol_to_coding_window(symbol, i)
-        packet = Packet(first_id=i, nb_id=1, key=None, data = symbol)
+    source_id = 0
+    for window_idx in range(MAX_CODING_WINDOW_SIZE*NB_WINDOW):
+        for i in range(MAX_CODING_WINDOW_SIZE):
+            symbol = make_source_symbol(source_id)
+            encoder.add_source_symbol_to_coding_window(symbol, source_id)
+            packet = Packet(first_id=source_id, nb_id=1, key=None, data=symbol)
+            source_id += 1
+            packet_list.append(packet)
+        encoder.generate_coding_coefs(key=window_idx, add_param=0)
+        repair_packet = encoder.build_repair_symbol()
+        packet_list.append(repair_packet)
+
+    for packet in packet_list:
         print(packet)
-        packet_list.append(packet)
 
-    (encoder.build_repair_symbol())
-    
-    encoder.generate_coding_coefs(key=2,add_param=1)
-
-    packet_list.append()
-    
-    
 #---------------------------------------------------------------------------
 
 if __name__ == "__main__":
